50262d87514a2f43af938e5a3eca4b92f976b569
kubot <kubotaren5213@docomo.ne.jp>
2024-08-07 01:04:58 +0900

    add:travel_plan作成画面の導線確保

--- app/controllers/travel_plans_controller.rb
+++ app/controllers/travel_plans_controller.rb
@@ -2,4 +2,17 @@ class TravelPlansController < ApplicationController
     def index
       @travel_plans = TravelPlan.all
     end
+
+    def new
+      @travel_plan = TravelPlan.new
+    end
+
+    def create
+      @travel_plan = current_user.travel_plans.build(travel_plan_params)
+      if @travel_plan.save
+        redirect_to travel_plans_path
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
 end
